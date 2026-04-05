8c5277807091b032727e3712eb6349954c37a03b
LukazAllan <allanlucas130@gmail.com>
2026-04-05 19:22:56 -0300

    Implementado (Model) Aluno

--- src/main/java/br/com/alunoonline/api/model/Aluno.java
+++ src/main/java/br/com/alunoonline/api/model/Aluno.java
@@ -1,4 +1,23 @@
 package br.com.alunoonline.api.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name="aluno")
+@Entity
 public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tipoDocumento;
+    private String numeroDocumento;
+    private String email;
+    private String telefone;
+
 }
