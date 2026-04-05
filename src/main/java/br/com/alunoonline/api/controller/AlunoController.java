ac13b0f04b4041b9dffd85c27cfd4bc9e295ef27
LukazAllan <allanlucas130@gmail.com>
2026-04-05 19:36:52 -0300

    Implementado AlunoController com 1 endpoint em getAllAlunos()

--- src/main/java/br/com/alunoonline/api/controller/AlunoController.java
+++ src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -1,4 +1,24 @@
 package br.com.alunoonline.api.controller;
 
+import br.com.alunoonline.api.model.Aluno;
+import br.com.alunoonline.api.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class AlunoController {
+
+    @Autowired
+    AlunoService alunoService;
+
+    @GetMapping("/alunos")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Aluno> getAllAlunos(){
+        return alunoService.getAllAlunos();
+    }
 }
